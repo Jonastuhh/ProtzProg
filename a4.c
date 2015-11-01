9f4e09aac299ea6cd1d3eb857cb57d5d7e5bd962
Jonastuhh <eckhoff-jonas@web.de>
2015-11-01 17:19:02 +0100

    Update a4.c

--- a4.c
+++ a4.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 
 int main (){
-
+    
 int a=1,n=9,b,m=n;
     while (n>0) {
-    b=-n+2;                 //
-    while (m>-a+1) {
-        
-        if (b<1) {
-        printf("  ");
+        b=-n+2;                 //b wird hochgezählt, nur Werte über 1 werden angezeigt
+        while (m>0) {
+            
+            if (b<1) {
+                printf("  ");
+            }
+            
+            else
+                printf("%d ",b);
+            m=m-1;
+            b=b+1;              //b wird hochgezählt bis es die Zeilennummer erreicht hat
+            
         }
-        
-        else
-        printf("%d ",b);
-        m=m-1;
-        b=b+1;              //b wird hochgezählt bis es die Zeilennummer erreicht hat
-        
-    }
-        b=b-2;              //Mitte erreicht, b wird einmalig erniedriegt
+        b=b-2;                  //Mitte erreicht, b wird einmalig erniedriegt
         
         while (b>0) {
             printf("%d ",b);
             b=b-1;
-        
+            
         }
-        printf("\n");       //Zeile wird abgeschlossen
-    n=n-1;                  //wichtig für die Anzahl der Leerzeichen
-    m=n;                    //
-    a=a+1;                  //zählt die aktuelle Zeile
+        printf("\n");           //Zeile wird abgeschlossen
+        n=n-1;                  //wichtig für die Anzahl der Leerzeichen und die Anzahl der Zeichen
+        m=n+a;                  //ergibt immer Anfangs-n
+        a=a+1;                  //zählt die aktuelle Zeile
     }
-
+    
 }
