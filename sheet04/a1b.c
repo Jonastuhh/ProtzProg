93c30367c17756d85032d958f7535af53f2676e5
Jonastuhh <eckhoff-jonas@web.de>
2015-11-12 16:30:17 +0100

    Update a1b.c

--- sheet04/a1b.c
+++ sheet04/a1b.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <stdint.h>
 
-long double k,x,y,e,z,l;
+long double k,x,y,e,z,l,Summe;
 long double f(long double x, int k){
 	
 	x=(x-1);
@@ -23,9 +23,19 @@ long double f(long double x, int k){
 
 int main(){
 	x=1;
-	k=2;
 	
+	
+	for (k=0; k<10; k=k+1) {
+       
+        y=f(x,k);
+        if (y<=e-20) {
+            k=10;
+        }
+        Summe=Summe+y;
+        
+    }
+
 	int y = f(x,k);
-	printf("ln(%lf) = %lf\n",x, y);
+	printf("ln(%lf) = %lf\n",x, Summe);
 	
 }
