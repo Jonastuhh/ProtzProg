f0ba2a61445a83c2db08f470d4ac2d872239ff9a
Jonastuhh <eckhoff-jonas@web.de>
2015-11-12 15:35:30 +0100

    Update a1.c

--- sheet04/a1.c
+++ sheet04/a1.c
@@ -1,21 +1,47 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdint.h>
 
-long double k,e,v=1,x,y;
+long double k,e,v=1,x,y,z,F,a,c,n,Summe;
 
 long double f(x,k){
+
     
-    for (e=0; e<k; e++) {
-        v=v*-1;
-        y=v;
+    z=x;
+    for (e=1; e<2*k+1; e=e+1) {
+        x=z*x;
+        }
+    y=x;
+    a=0;
+    c=1;
+    n=2*k+1;
+    do {
+        c=c*(a+1);
+        a=a+1;
+    } while (a<n);
+
+    F=c;
+    y=y/F;
+    
+    if (k%2==1) {
+        y=-y;
     }
     return y;
 }
 
 int main (){
-    x=1;
-    k=3;
-    y=f(x,k);
-    printf("%Lf",y);
+    x=2.0;
+    k=10;
+    for (k; k>=0; k=k-1) {
+        y=f(x,k);
+        Summe=Summe+y;
+    }
+    
+    
+    
+    
+    printf("%.20Lf",Summe);
     printf("\n");
+    return 0;
 }
+
