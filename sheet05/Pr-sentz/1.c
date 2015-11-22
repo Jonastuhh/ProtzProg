21b1b6a4da99d53458f9f187de90037f0b57cf26
lennard963 <lennard-benecke@web.de>
2015-11-22 16:47:16 +0100

    Create 1.c

---
+++ sheet05/Pr-sentz/1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define NMAX 3
+
+ int dotprod(int A[NMAX],int B[NMAX]){
+	int i,s=0;
+	for(i=0;i<NMAX;i++){
+	s+=A[i]*B[i];
+	}
+	return(s);
+}
+int main (){
+	int s,i;
+	int A[NMAX],B[NMAX];
+	for(i=0;i<NMAX;i++){
+		scanf("%d",&A[i]);
+	}
+	for(i=0;i<NMAX;i++){
+		scanf("%d",&B[i]);	
+	}
+	dotprod(A[NMAX],B[NMAX]);
+	printf("%d",s);
+	return(0);
+}
