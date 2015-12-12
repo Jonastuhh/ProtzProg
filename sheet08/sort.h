1ade8be908be52e434d777ca93948b0c8e4365fc
Jonastuhh <eckhoff-jonas@web.de>
2015-12-12 22:41:13 +0100

    Create sort.h

---
+++ sheet08/sort.h
@@ -0,0 +1,11 @@
+#ifndef sort_h
+#define sort_h
+
+#include <stdio.h>
+#include "sort.c"
+
+void bubblesort(int Ar[],int size);
+void shakersort(int *Ar,int size);
+void insertionsort(int *Ar,int size);
+
+#endif /* sort_h */
